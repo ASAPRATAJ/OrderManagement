fbb3ba416859d6eba8155f4d12727681afdf14d8
ASAPRATAJ <bartosz.ratajewski@gmail.com>
2024-09-09 15:41:51 +0200

    Added functionality: JWT Authorization.

--- OrderManagementBackend/orders/views.py
+++ OrderManagementBackend/orders/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .serializers import OrderCreateSerializer, OrderListSerializer
-from .models import Order, OrderProduct
+from .models import Order
 
 
 class OrderCreateView(generics.CreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderCreateSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class OrderListView(generics.ListAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderListSerializer
+    permission_classes = [IsAuthenticated]
--- OrderManagementBackend/users/urls.py
+++ OrderManagementBackend/users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserCreateView, UserListView
+
 
 urlpatterns = [
     path('users/create/', UserCreateView.as_view(), name='user-create'),
     path('users/', UserListView.as_view(), name='user-list'),
+    path('users/token/create/', TokenObtainPairView.as_view(), name='token-create'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
