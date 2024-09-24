9833f4b41c2693d595eceaa4c0df9d09e080f9b5
ASAPRATAJ <bartosz.ratajewski@gmail.com>
2024-09-24 21:22:18 +0200

    Added field 'tag' to product.

--- OrderManagementBackend/products/models.py
+++ OrderManagementBackend/products/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
+tags = ['Podstawowa', 'Rozszerzona', 'Premium', 'Smaki tygodnia']
+
 
 class Product(models.Model):
+    TAG_CHOICES = [
+        ('basic', 'Podstawowa'),
+        ('extend', 'Rozszerzona'),
+        ('premium', 'Premium'),
+        ('weekly_flavors', 'Smaki tygodnia'),
+    ]
+
     title = models.CharField(max_length=120, blank=False)
     description = models.TextField()
     price = models.IntegerField()
     image = models.ImageField(upload_to='products/images/', null=True, blank=True,
                               default='products/images/PolishLodyLogo.jpg')
     is_active = models.BooleanField(default=True)
+    tag = models.CharField(max_length=50, choices=TAG_CHOICES, default='basic')
 
     def __str__(self):
         return self.title
