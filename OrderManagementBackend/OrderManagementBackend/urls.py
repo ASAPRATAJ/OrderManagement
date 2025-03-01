f5371bcec0a8443e4751bef4ae50d533ee8a058f
ASAPRATAJ <bartosz.ratajewski@gmail.com>
2025-03-01 01:42:01 +0100

    Deploy to railway- added media url.

--- OrderManagementBackend/OrderManagementBackend/urls.py
+++ OrderManagementBackend/OrderManagementBackend/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
